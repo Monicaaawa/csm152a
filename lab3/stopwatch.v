c51488dc39e4015fbcb9ead3143a80ffbedc7a75
Shiyu Ye <105144733+Monicaaawa@users.noreply.github.com>
2025-02-10 11:36:28 -0800

    fix assignment

--- lab3/stopwatch.v
+++ lab3/stopwatch.v
@@ -6,22 +6,22 @@ module stopwatch(
 
     always @(posedge clk_1Hz or posedge rst) begin
         if (rst) begin
-            sec_ones = 0; sec_tens = 0;
-            min_ones = 0; min_tens = 0;
-            paused = 0;
+            sec_ones <= 0; sec_tens <= 0;
+            min_ones <= 0; min_tens <= 0;
+            paused <= 0;
         end else if (!pause) begin
             if (!adj) begin
-                sec_ones = sec_ones + 1;
+                sec_ones <= sec_ones + 1;
                 if (sec_ones == 9) begin
-                    sec_ones = 0;
-                    sec_tens = sec_tens + 1;
+                    sec_ones <= 0;
+                    sec_tens <= sec_tens + 1;
                     if (sec_tens == 5) begin
-                        sec_tens = 0;
-                        min_ones = min_ones + 1;
+                        sec_tens <= 0;
+                        min_ones <= min_ones + 1;
                         if (min_ones == 9) begin
-                            min_ones = 0;
-                            min_tens = min_tens + 1;
-                            if (min_tens == 5) min_tens = 0;
+                            min_ones <= 0;
+                            min_tens <= min_tens + 1;
+                            if (min_tens == 5) min_tens <= 0;
                         end
                     end
                 end
@@ -32,18 +32,18 @@ module stopwatch(
     always @(posedge clk_2Hz) begin
         if (adj) begin
             if (sel) begin
-                sec_ones = sec_ones + 1;
+                sec_ones <= sec_ones + 1;
                 if (sec_ones == 9) begin
-                    sec_ones = 0;
-                    sec_tens = sec_tens + 1;
-                    if (sec_tens == 5) sec_tens = 0;
+                    sec_ones <= 0;
+                    sec_tens <= sec_tens + 1;
+                    if (sec_tens == 5) sec_tens <= 0;
                 end
             end else begin
-                min_ones = min_ones + 1;
+                min_ones <= min_ones + 1;
                 if (min_ones == 9) begin
-                    min_ones = 0;
-                    min_tens = min_tens + 1;
-                    if (min_tens == 5) min_tens = 0;
+                    min_ones <= 0;
+                    min_tens <= min_tens + 1;
+                    if (min_tens == 5) min_tens <= 0;
                 end
             end
         end
